c5f7960739331a46d60003b41358ddb695a4392d
Yejin Chung <131370777+Chungyejin@users.noreply.github.com>
2026-03-22 22:21:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from functions.lerArquivo import ler_arquivo
-from functions.parseexpressao import parseExpressao
+from functions.parseEspressao import parseExpressao
 from functions.gerarAssembly import gerarAssembly
+from functions.exibirResultados import exibirResultados  
 
-
-print(f"Arquivo: {ler_arquivo("arquivosTeste/teste.txt")}\n")
+print(f"Arquivo: {ler_arquivo('arquivosTeste/teste.txt')}\n")
 arq = ler_arquivo("arquivosTeste/teste.txt")
 
 valid_tokens = []
+resultados = [] 
+
 for line in arq:
     line_tokens = parseExpressao(line)
     print(line_tokens)
-    valid_tokens = valid_tokens + parseExpressao(line)
 
+    valid_tokens = valid_tokens + parseExpressao(line)
+    resultados.append(len(line_tokens))
 
 print(gerarAssembly(valid_tokens))
+
+exibirResultados(resultados)
