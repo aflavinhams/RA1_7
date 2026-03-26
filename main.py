bf77a123be491b2998a35b266041160a37969321
Isabella <berkembrockisabella@gmail.com>
2026-03-26 17:05:04 -0300

    Ajustes de compatibilidade com arquitetura ARMv7 realizadas no gerarAssembly

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 from functions.lerArquivo import ler_arquivo
 from functions.parseexpressao import parseExpressao
-from functions.gerarAssembly import gerarAssembly
+from functions.gerarAssembly import gerarAssembly, criarState, finalizarAssembly
 from functions.exibirResultados import exibirResultados
 from functions.executarExpressao import executarExpressao
 
-print(f"Arquivo: {ler_arquivo('arquivosTeste/teste.txt')}\n")
-arq = ler_arquivo("arquivosTeste/teste.txt")
+print(f"Arquivo: {ler_arquivo('arquivosTeste/teste2.txt')}\n")
+arq = ler_arquivo("arquivosTeste/teste2.txt")
 
-valid_tokens = []
-resultados = []
 memoria = {}
 resultados_py = {}
 linha = 1
 
+state = criarState()  # cria o estado compartilhado entre todas as linhas
+
 for line in arq:
     line_tokens = parseExpressao(line)
     print(line_tokens)
+
     executarExpressao(line_tokens, memoria, resultados_py, linha)
+    gerarAssembly(line_tokens, state, linha)  # passa o número da linha atual
+
     linha += 1
 
-    valid_tokens = valid_tokens + parseExpressao(line)
-    resultados.append(len(line_tokens))
-
-print(valid_tokens)
-print(gerarAssembly(valid_tokens))
+print(finalizarAssembly(state))  # gera o .data e exibe o assembly completo
 
 print("===== EXECUTAR EXPRESSAO =====")
 print(memoria)
 print(resultados_py)
 
-exibirResultados(resultados_py) 
+exibirResultados(resultados_py)
